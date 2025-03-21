ffc34a90e2a7b7768eee8586b8b42ca0c37c6788
Irene Semenikhina <irene@MacBook-Air-Irene.local>
2025-03-21 12:47:40 +0100

    Refresh every hour

--- components/DateAndTime.js
+++ components/DateAndTime.js
@@ -1,7 +1,14 @@
 import { getWeekDay, getTime, getAMPM } from "../services/helpers";
 import styles from "./DateAndTime.module.css";
 
-export const DateAndTime = ({ weatherData, unitSystem }) => {
+export const DateAndTime = ({ weatherData, unitSystem, refreshData }) => {
+  useEffect(() => {
+    const interval = setInterval(() => {
+      refreshData();
+    }, 3600000); // Refresh every hour
+
+    return () => clearInterval(interval);
+  }, [refreshData]);
   return (
     <div className={styles.wrapper}>
       <h2>
